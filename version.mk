02191ef4d24c2b60779eb928061c14a0100153a0
Chris Packham <judge.packham@gmail.com>
2010-01-18 16:43:18 -0500

    version.mk: allow passing of VERSION from make
    So that we can build tarballs.

--- version.mk
+++ version.mk
@@ -1,11 +1,16 @@
 # This makefile generates a version.cpp file based on version.cpp.in
 # replacing some tags with values computed at build time.
 
+# For tarball builds we want to be able to pass the version information in
+VERSION=$$(VERSION)
+isEmpty( VERSION ) {
+	VERSION=$$system(git describe --always)
+}
+
 VERSION_CPP_IN = version.cpp.in
 version.output = ${QMAKE_FILE_BASE}
-version.commands = sed -e \"s/@@VERSION@@/`git describe 2> /dev/null \
-	|| git rev-parse --short HEAD`/g\" \
-	-e \"s/@@DATE@@/`date --utc`/g\" < ${QMAKE_FILE_NAME} > ${QMAKE_FILE_OUT}
+version.commands = sed -e \"s/@@VERSION@@/$${VERSION}/g\" \
+		-e \"s/@@DATE@@/`date`/g\" < ${QMAKE_FILE_NAME} > ${QMAKE_FILE_OUT}
 version.input = VERSION_CPP_IN
 version.CONFIG += no_link
 version.variable_out = SOURCES
